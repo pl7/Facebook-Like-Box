c1da107c1591238dea8d575ecf9c0bc740961b8d
Pascal Link <admin@pl07.de>
2012-10-20 20:28:07 +0200

    inital Commit
    Display facebook like box as extension for my joomla 2.5 template
    'meinVerein'

---
+++ tmpl/default.php
@@ -0,0 +1,13 @@
+<?php defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
+<?php if($show_fb_box == 1) :?>
+<section id="fb_box_plugin" class="sidebar">
+<div class="fb-box-plugin">
+    <header>
+        <h3>Werde eine Fan vom SVW!</h3>
+    </header>    
+    <div class="fb-like">
+        <fb:like-box href="http://www.facebook.com/svwiesbaden" width="250" show_faces="false" border_color="#fff" stream="true" header="true"></fb:like-box>
+    </div>
+</div>
+</section>
+<?php endif;?>
